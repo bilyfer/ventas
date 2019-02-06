9e4374402571c7c77d1cb6e516a87518e9a34d05
billyafernandez <billy@acklenavenue.com>
2019-02-05 19:46:12 -0600

    agregando modelo controlador y vista para Alumnos

---
+++ MVC/Controllers/AlumnosController.cs
@@ -0,0 +1,22 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            var alumno = new Alumno();
+            alumno.Id = 1;
+            alumno.Nombre = "Steve Rogers";
+
+            return View(alumno);
+        }
+    }
+}
