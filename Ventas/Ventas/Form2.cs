90fe427bc4b738db15171823c8f5143367b71d79
billyafernandez <billy@acklenavenue.com>
2019-02-22 08:49:22 -0600

    Diseñando el formulario de mantenimiento de productos

---
+++ Ventas/Ventas/Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ventas.Modelos;
+
+namespace Ventas
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            var producto1 = new Producto("Laptop", 20000);
+            producto1.AumentarPrecio(20);
+
+            MessageBox.Show(producto1.ObtenerInfo());
+
+            //MessageBox.Show(producto1.Id.ToString());
+
+        }
+    }
+}
